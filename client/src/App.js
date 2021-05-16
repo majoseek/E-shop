70a23ba78d7b0c94f4bcd5b47fdd16f004be0c36
majoseek <matuslaw.mati@gmail.com>
2021-05-16 22:24:48 +0200

    Connected client with server

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./components/styles/App.css";
 import Product from "./components/Product";
@@ -6,51 +6,22 @@ import CardColumns from "react-bootstrap/CardColumns";
 import "./components/Menu";
 import Menu from "./components/Menu";
 import { useState } from "react";
-const games = [
-    {
-        name: "Counter Strike: Global-Offensive",
-        price: 25,
-        img_url:
-            "https://cdn.akamai.steamstatic.com/steam/apps/730/capsule_616x353.jpg?t=1612812939",
-    },
-    {
-        name: "Dragon Ball Z:Kakarot",
-        price: 96,
-        img_url: "https://i.ytimg.com/vi/kGhegZ4eCGY/maxresdefault.jpg",
-    },
-    {
-        name: "Pokémon Masters EX",
-        price: 132,
-        img_url:
-            "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_masters/pokemon-masters-ex-169.jpg",
-    },
-    {
-        name: "Super Mario Bros",
-        price: 1,
-        img_url:
-            "https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/virtual_console_nintendo_3ds_7/SI_3DSVC_SuperMarioBros.jpg",
-    },
-    {
-        name: "Fortnite",
-        price: "FREE",
-        img_url:
-            "https://compass-ssl.xbox.com/assets/54/f9/54f9ed2b-1092-4e79-9bec-333f913d918f.jpg?n=Fortnite_gallery-0_1350x759_01.jpg",
-    },
-    {
-        name: "Minecraft",
-        price: 512,
-        img_url:
-            "https://gamerweb.pl/wp-content/uploads/minecraft-e1488210750250.jpg",
-    },
-    {
-        name: "Terraria",
-        price: 25,
-        img_url: "https://i.ytimg.com/vi/oCNiYRNI_UM/maxresdefault.jpg",
-    },
-];
+import axios from "axios";
+
 function App() {
+    const [games, setGames] = useState([]);
     const [products, setProducts] = useState([]);
     const [total_price, setTotalPrice] = useState(0);
+    useEffect(() => {
+        axios
+            .get("/testing")
+            .then(function (response) {
+                setGames(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    });
     const add_product = (product) => {
         setTotalPrice(total_price + product.price);
         const exist = products.find((x) => x.name === product.name);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,54 @@
 import express from "express";
+import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3001;
+const games = [
+    {
+        name: "Counter Strike: Global-Offensive",
+        price: 25,
+        img_url:
+            "https://cdn.akamai.steamstatic.com/steam/apps/730/capsule_616x353.jpg?t=1612812939",
+    },
+    {
+        name: "Dragon Ball Z:Kakarot",
+        price: 96,
+        img_url: "https://i.ytimg.com/vi/kGhegZ4eCGY/maxresdefault.jpg",
+    },
+    {
+        name: "Pokémon Masters EX",
+        price: 132,
+        img_url:
+            "https://assets.pokemon.com/assets/cms2/img/video-games/video-games/pokemon_masters/pokemon-masters-ex-169.jpg",
+    },
+    {
+        name: "Super Mario Bros",
+        price: 1,
+        img_url:
+            "https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/virtual_console_nintendo_3ds_7/SI_3DSVC_SuperMarioBros.jpg",
+    },
+    {
+        name: "Fortnite",
+        price: "FREE",
+        img_url:
+            "https://compass-ssl.xbox.com/assets/54/f9/54f9ed2b-1092-4e79-9bec-333f913d918f.jpg?n=Fortnite_gallery-0_1350x759_01.jpg",
+    },
+    {
+        name: "Minecraft",
+        price: 512,
+        img_url:
+            "https://gamerweb.pl/wp-content/uploads/minecraft-e1488210750250.jpg",
+    },
+    {
+        name: "Terraria",
+        price: 25,
+        img_url: "https://i.ytimg.com/vi/oCNiYRNI_UM/maxresdefault.jpg",
+    },
+];
 app.get("/", (req, res) => {
-    res.send("test123");
+    res.send("hi");
 });
 app.get("/testing", (req, res) => {
-    res.send("123:)");
+    res.send(games);
 });
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
