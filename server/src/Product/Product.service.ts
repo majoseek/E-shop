09032f5a8d8b4604bca9218729e24bf0368aac2c
majoseek <matuslaw.mati@gmail.com>
2021-06-12 20:27:21 +0200

    Added docs for services

--- server/src/Product/Product.service.ts
+++ server/src/Product/Product.service.ts
@@ -1,6 +1,11 @@
 import client from "../database";
 import Product from "./Product.interface";
 import jwt from "jsonwebtoken";
+
+/**
+ *
+ * @returns Promise<Array<Product>> - contains all the products, which are avaible in the store
+ */
 export const get_games = async (): Promise<Array<Product>> => {
     const collection = await client
         .db("Shop")
@@ -9,18 +14,27 @@ export const get_games = async (): Promise<Array<Product>> => {
         .toArray();
     return collection;
 };
+
+/**
+ *
+ * @param token - string, which contains auth token
+ * @param products - Array<Product>, contains products to purchase for given user
+ * @returns
+ */
 export const checkout = async (
     token: any,
     products: Array<Product>
 ): Promise<Object> => {
     return new Promise((resolver, reject) => {
         jwt.verify(
+            //check if token is correct
             token,
             process.env.JWT_SECRET as string,
             (err: any, decoded: any) => {
                 if (err) resolver("Wrong username or password");
                 else {
                     products.forEach((product: Product) => {
+                        //purchase all of the products
                         client
                             .db("Shop")
                             .collection("Games")
--- server/src/User/User.service.ts
+++ server/src/User/User.service.ts
@@ -2,6 +2,13 @@ import jwt from "jsonwebtoken";
 import client from "../database";
 import bcrypt from "bcrypt";
 import User from "./User.interface";
+
+/**
+ *
+ * @param username string containing username of user, which tries to log in
+ * @param password string containing password of user, which tries to log in
+ * @returns Promise<Object> login status, which tells if user data is correct
+ */
 export const login = async (
     username: string,
     password: string
@@ -50,12 +57,18 @@ export const login = async (
     }
 };
 
+/**
+ *
+ * @param user interface:User, contains user, which tries to register
+ * @returns Promise<Object> register status, which tells if user successfully registered
+ */
 export const register = async (user: User): Promise<Object> => {
     return new Promise((resolver, reject) => {
         client
             .db("Shop")
             .collection("Users")
             .findOne({ email: user.email }, (err, result) => {
+                //check if user with that email exists
                 if (err) console.log(err);
                 if (result) {
                     resolver({
@@ -66,11 +79,12 @@ export const register = async (user: User): Promise<Object> => {
                         .db("Shop")
                         .collection("Users")
                         .insertOne({
+                            //register user
                             email: user.email,
                             username: user.username,
                             password: user.password,
                         })
-                        .then((result) =>
+                        .then(() =>
                             resolver({
                                 message: "User registered successfully",
                             })
